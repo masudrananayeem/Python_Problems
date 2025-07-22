129c9cb508dd587baae0888f76646c3c00621735
Masud Rana Nayeem <65963574+masudrananayeem@users.noreply.github.com>
2025-07-22 22:20:41 +0600

    Add files via upload

---
+++ Module_3/M3_Problem_2.py
@@ -0,0 +1,13 @@
+class Animal:
+    def sound(m):
+        print('Animals make different sounds.') 
+
+class Dog(Animal):
+    def sound(m):
+        print('Dog barks!')  
+
+animal=Animal()
+dog=Dog()
+
+animal.sound()  
+dog.sound()    
